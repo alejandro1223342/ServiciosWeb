4eae44f7f7749ee85878b75d9b4b98a355ffeac5
Alejandro Muñoz <alejomuoss@gmail.com>
2023-06-22 17:37:42 -0500

    Se agrega ApiClient

--- app/src/main/java/com/example/serviciosweb/ApiClient.java
+++ app/src/main/java/com/example/serviciosweb/ApiClient.java
@@ -1,4 +1,19 @@
 package com.example.serviciosweb;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class ApiClient {
+    private static final String BASE_URL = "https://apifunciones.azurewebsites.net/";
+    private static Retrofit retrofit = null;
+
+    public static Retrofit getClient() {
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
 }
